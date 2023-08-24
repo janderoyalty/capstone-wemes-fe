f71154466c0bc7c49f395c4cb828a677d6317caf
Jande Royalty <janderoyalty@gmail.com>
2023-08-24 02:50:34 -0500

    Adds last four to dropdown in AddTransactionModal

--- src/Components/Dropdown/CustomerDropdown.js
+++ src/Components/Dropdown/CustomerDropdown.js
@@ -3,16 +3,18 @@ import Select from "react-select";
 
 const CustomerDropdown = ({ data, isAdmin, onSelect, person }) => {
   const [searchTerm, setSearchTerm] = useState("");
+
   const filteredData = data.filter(
     (item) =>
       item.admin === isAdmin &&
       (item.first_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.last_name.toLowerCase().includes(searchTerm.toLowerCase()))
+        item.last_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.last_four.includes(searchTerm))
   );
 
   const options = filteredData.map((item) => ({
     value: item.id,
-    label: `${item.first_name} ${item.last_name}`,
+    label: `${item.first_name} ${item.last_name} ${item.last_four}`,
   }));
 
   return (
